dd2b875ee581adaba726a7c06f4fb923f9b4c713
Juan Carlos Santiago <83483568+JuanCarlos712@users.noreply.github.com>
2025-11-11 21:22:44 -0600

    Register TrainedModel with Django admin

--- detection_app/admin.py
+++ detection_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TrainedModel
 
-# Register your models here.
+@admin.register(TrainedModel)
+class TrainedModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'f1_score', 'precision', 'recall', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['name']
